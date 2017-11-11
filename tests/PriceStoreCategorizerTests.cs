2789e8cc667bbd29f50cc906cdd99dac02516d87
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-11-11 10:15:44 -0200

    Watch adicionado, criando primeiro teste

--- tests/PriceStoreCategorizerTests.cs
+++ tests/PriceStoreCategorizerTests.cs
@@ -1,14 +1,20 @@
 using System;
 using Xunit;
+using PriceStoreCategorizer.Controllers;
+using Microsoft.AspNetCore.Mvc;
 
 namespace tests
 {
     public class PriceStoreCategorizerTests
     {
         [Fact]
-        public void Test1()
+        public void Dado_Um_Produto_Sem_Nome_Entao_Retorn_BadRequest()
         {
+            var sut = new ProductCategoriesAutoBinderController(null);
 
+            var result = sut.Post(null);
+
+            Assert.Equal(400, (result as ObjectResult).StatusCode);
         }
     }
 }
