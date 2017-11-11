8948e905cb0f8fa1bb88f01937d6cf19cc50eafd
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2017-11-11 09:26:05 -0200

    Criada interface ProductBinder com metodo de AutoBind

--- src/Controllers/ProductCategoriesAutoBinderController.cs
+++ src/Controllers/ProductCategoriesAutoBinderController.cs
@@ -4,17 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using PriceStoreCategorizer.ViewModels;
+using PriceStoreCategorizer.ApplicationServices.Interfaces;
 
 namespace PriceStoreCategorizer.Controllers
 {
     [Route("api/[controller]")]
     public class ProductCategoriesAutoBinderController : Controller
     {
+        private IProductBinderAppService _productBinderAppService;
+
+        public ProductCategoriesAutoBinderController(IProductBinderAppService productBinderAppService)
+        {
+            _productBinderAppService = productBinderAppService;
+        }
         
         [HttpPost]
         public void Post([FromBody]string productName)
         {
-            //_productBinderAppService.AutoBind(productName);
+            _productBinderAppService.AutoBind(productName);
         }
     }
 }
