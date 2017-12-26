12744aa7b4901c32e5c1f2964a12ded705a5e8a6
Rafael-Miceli <rafael.miceli@hotmail.com>
2017-12-26 19:06:14 -0200

    Criado teste para começar mapeamento de produtos por categorias baby step

--- tests/PriceStoreCategorizerTests.cs
+++ tests/PriceStoreCategorizerTests.cs
@@ -9,13 +9,23 @@ namespace tests
     public class PriceStoreCategorizerTests
     {
         [Fact]
-        public void Dado_Um_Produto_Sem_Nome_Entao_Retorn_BadRequest()
+        public void Dado_Um_Produto_Sem_Nome_Entao_Retorne_BadRequest()
         {
             var sut = new ProductCategoriesAutoBinderController(new ProductBinderAppService());
 
             var result = sut.Post(null);
 
             Assert.Equal(400, (result as BadRequestResult).StatusCode);
+        }
+
+        [Fact]
+        public void Dado_O_Produto_Tomate_Entao_Retorne_A_Categoria_Legumes()
+        {
+            var sut = new ProductBinderAppService();
+
+            var result = sut.GetProductCategory("Tomate");
+
+            Assert.Equal("Legumes", result);
         }
     }
 }
