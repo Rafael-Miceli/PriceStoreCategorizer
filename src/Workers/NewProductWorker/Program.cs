b0a46bb9a2260350b7fd90af7c6d4da2ac770d01
Rafael-Miceli <rafael.miceli@hotmail.com>
2018-01-26 08:50:17 -0200

    Recebendo mensagens de exchange

--- src/Workers/NewProductWorker/Program.cs
+++ src/Workers/NewProductWorker/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
 namespace PriceStoreCategorizer.NewProductWorker
 {
@@ -6,7 +9,33 @@ namespace PriceStoreCategorizer.NewProductWorker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory() { HostName = "localhost" };            
+            using(var connection = factory.CreateConnection())
+            using(var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(exchange: "newproduct", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+                channel.QueueBind(queue: queueName,
+                                exchange: "newproduct",
+                                routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for newproduct.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] {0}", message);
+                };
+                channel.BasicConsume(queue: queueName,
+                                    autoAck: true,
+                                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();
+            }
         }
     }
 }
