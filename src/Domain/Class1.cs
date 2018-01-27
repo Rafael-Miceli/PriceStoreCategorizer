7debd2e7c6c5c5d8a321acbf6f53dd26e43ff737
Rafael de Mello Miceli Paiva <rafael.miceli@hotmail.com>
2018-01-27 13:08:23 -0200

    Definido produto dominio

--- src/Domain/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace PriceStoreCategorizer.Domain
-{
-    public class Class1
-    {
-    }
-}
---
+++ src/Domain/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace PriceStoreCategorizer.Domain
+{
+    public class Product
+    {
+        public Product(string name)
+        {
+            Name = name;
+            CategoryName = "Sem Categoria";
+        }
+
+        public string Name { get; private set; }
+        public string CategoryName { get; private set; }
+
+        public void CategorizeProduct(string categoryName)
+        {
+            CategoryName = categoryName;
+        }
+    }
+}
